85e35680b623fb62caf8533b1fc430e4ef028ccc
rxxuzi <75666622+rxxuzi@users.noreply.github.com>
2023-12-30 16:53:46 +0900

    Set upper limit

--- src/main/java/main/WebCrawler.java
+++ src/main/java/main/WebCrawler.java
@@ -14,6 +14,38 @@ import java.util.List;
 public class WebCrawler {
 
     public static void run(List<String> urls) {
+        run(urls, Integer.MAX_VALUE);
+    }
+
+    public static void run(List<String> urls, int max) {
+        System.setProperty("webdriver.chrome.driver", Fast.DRIVER_PATH);
+        WebDriver driver = new ChromeDriver();
+        driver.get(Fast.FIRST_VISIT_URL);
+        WebElement element = driver.findElement(By.id("LoginLink"));
+        element.click();
+
+        Login.login(driver);
+        int i = 0;
+        for (String url : urls) {
+            try{
+                driver.get(Fast.BASE_URL + url);
+                WebElement box = driver.findElement(By.className("common_left_box"));
+                String htmlContent = box.getAttribute("outerHTML");
+                String title = box.findElement(By.tagName("h1")).getText();
+
+                Datasets.add(new Data(title, htmlContent, Fast.BASE_URL + url));
+            }catch (Exception e){
+                Datasets.saveJson("./data/data" + System.currentTimeMillis()  + ".json");
+            }
+            System.out.println(i + " : "+url);
+            i++;
+            if (i > max) break;
+        }
+        driver.close();
+    }
+
+    public static void main(String[] args) {
+        List<String> urls = List.of("/entry_sheets/40006","/entry_sheets/38547");
         System.setProperty("webdriver.chrome.driver", Fast.DRIVER_PATH);
         WebDriver driver = new ChromeDriver();
         driver.get(Fast.FIRST_VISIT_URL);
@@ -36,5 +68,10 @@ public class WebCrawler {
             System.out.println(i + " : "+url);
             i++;
         }
+        driver.close();
+        Datasets.saveJson("./data/testdata.json");
     }
+
+
+
 }
